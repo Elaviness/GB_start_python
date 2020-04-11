36f0d21b37340e24dcfa7fa608b958024508c067
Lafi <elaviness@yandex.ru>
2020-04-11 14:00:10 +0300

    add first exercise

---
+++ fst_ex.py
@@ -0,0 +1,29 @@
+# создание и вывод собственных переменных
+
+num_int = 55
+num_float = 42.035
+string = 'Первое задание'
+var_none = None
+
+print(num_int)
+print(type(num_int))
+
+print(num_float)
+print(type(num_float))
+
+print(num_int)
+print(type(string))
+
+print(var_none)
+print(type(var_none))
+
+# Ввод данных пользователем
+
+var_num = input('Введите любое число: ')
+var_str = input('Введите любую строку: ')
+
+print(var_num)
+print(type(var_num))
+
+print(var_str)
+print(type(var_str))
