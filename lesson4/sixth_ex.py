3434000a2b0a190aba5f2659f02a705bd1b56d64
Lafi <elaviness@yandex.ru>
2020-04-22 10:48:15 +0300

    add decision for sixth exercise

--- lesson4/sixth_ex.py
+++ lesson4/sixth_ex.py
@@ -3,3 +3,18 @@
 б) бесконечный итератор, повторяющий элементы некоторого списка, 
 определенного заранее.
 Подсказка: использовать функцию count() и cycle() модуля itertools."""
+
+from itertools import count,cycle
+
+def iter_num(n=int):
+    """ Бесконечный итератор целых чисел"""
+    for itm in count(n):
+        print(itm)
+
+def iter_string(string=str):
+    """ Бесконечный итератор повторяющий элементы входной строки"""
+    for itm in cycle(string):
+        print(itm)
+
+iter_num(5)
+iter_string("ABC")
