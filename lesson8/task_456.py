65f8202c86dc89a04c6d93e85be9c0e7fc55d979
Elaviness <48585255+Elaviness@users.noreply.github.com>
2020-05-11 15:19:07 +0300

    add fifth task

--- lesson8/task_456.py
+++ lesson8/task_456.py
@@ -16,23 +16,48 @@
 """
 from abc import ABC, abstractmethod
 
+
 class Storehouse:
     __equipment = {
         'printer': [],
         'scanner': [],
         'copier': []
     }
+    __structure_equipment = {
+
+    }
+
+    def get_equip(self):
+        return f'{self.__equipment}'
+
+    def get_struct_equip(self):
+        return f'{self.__structure_equipment}'
+
+    def add_equipment(self, eq_type, name, count):
+        self.__equipment[eq_type].append([name, count])
+
+    def transfer_equipment(self, structure, eq_type, name, count):
+        if structure not in self.__structure_equipment:
+            self.__structure_equipment.update({structure: {}})
+        if eq_type not in self.__structure_equipment[structure]:
+            self.__structure_equipment[structure].update({eq_type: []})
+        self.__structure_equipment[structure][eq_type].append([name, count])
 
 class OfficeEquipment(ABC):
 
     @property
     @abstractmethod
-    def __type(self) -> str:
+    def type(self) -> str:
         pass
 
     @property
     @abstractmethod
-    def __name(self) -> str:
+    def name(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def count(self) -> int:
         pass
 
     @abstractmethod
@@ -41,25 +66,78 @@ class OfficeEquipment(ABC):
 
 class Printer(OfficeEquipment):
     def __init__(self, **kwargs):
-        __type = 'printer'
-        __name = kwargs['name']
-        _is_color = kwargs['is_color']
-        _has_xerox = kwargs['xerox']
+        self.__type = 'printer'
+        self.__name = kwargs['name']
+        self.__count = kwargs['count']
+        self._is_color = kwargs['is_color']
+        self._has_xerox = kwargs['xerox']
+
+    @property
+    def type(self):
+        return f'{self.__type}'
+    
+    @property
+    def name(self):
+        return f'{self.__name}'
+    
+    @property
+    def count(self):
+        return f'{self.__count}'
+    
+    def add_itm(self, **kwargs):
+       self.__dict__.update(kwargs)      
 
 class Scanner(OfficeEquipment):
     def __init__(self, **kwargs):
-        __type = 'scanner'
-        __name = kwargs['name']
-        _connect_type = kwargs['connect']
+        self.__type = 'scanner'
+        self.__name = kwargs['name']
+        self.__count = kwargs['count']
+        self._connect_type = kwargs['connect']
+
+    @property
+    def type(self):
+        return f'{self.__type}'
+    
+    @property
+    def name(self):
+        return f'{self.__name}'
+    
+    @property
+    def count(self):
+        return f'{self.__count}'
+
+    def add_item(self, **kwargs):
+       self.__dict__.update(kwargs)    
 
 class Copier(OfficeEquipment):
     def __init__(self, **kwargs):
-        __type = 'copier'
-        __name = kwargs['name']
-        _is_color = kwargs['is_color']
+        self.__type = 'copier'
+        self.__name = kwargs['name']
+        self.__count = kwargs['count']
+        self._is_color = kwargs['is_color']
+    
+    @property
+    def type(self):
+        return f'{self.__type}'
+    
+    @property
+    def name(self):
+        return f'{self.__name}'
+    
+    @property
+    def count(self):
+        return f'{self.__count}'
 
+    def add_item(self, **kwargs):
+       self.__dict__.update(kwargs)  
 
-
+my_printer = Printer(name = 'epson', count = 5, is_color = True, xerox = False)
+print(my_printer.type)
+my_storehouse = Storehouse()
+my_storehouse.add_equipment(my_printer.type, my_printer.name, my_printer.count)
+print(my_storehouse.get_equip())
+my_storehouse.transfer_equipment('бухгалтерия',my_printer.type, my_printer.name, 2 )
+print(my_storehouse.get_struct_equip())
 
 
 
