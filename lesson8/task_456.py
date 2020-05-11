61edbc773e1de211ba2eb894887d48b773b700c8
Elaviness <48585255+Elaviness@users.noreply.github.com>
2020-05-11 15:59:26 +0300

    add 6th task

--- lesson8/task_456.py
+++ lesson8/task_456.py
@@ -15,6 +15,15 @@
 Подсказка: постарайтесь по возможности реализовать в проекте «Склад оргтехники» максимум возможностей, изученных на уроках по ООП.
 """
 from abc import ABC, abstractmethod
+from copy import deepcopy
+
+class CountIsNotDigit(Exception):
+    def __str__(self):
+        return 'Количество техники не может быть нецелым числом.'
+
+class CountOutOfLimit(Exception):
+    def __str__(self):
+        return 'Нельзя передать в структуру больше единиц, чем есть на складе.'
 
 
 class Storehouse:
@@ -34,14 +43,34 @@ class Storehouse:
         return f'{self.__structure_equipment}'
 
     def add_equipment(self, eq_type, name, count):
-        self.__equipment[eq_type].append([name, count])
+        try:
+            if not count.isdigit():
+                raise CountIsNotDigit
+            else:
+                count = int(count)
+        except CountIsNotDigit as error:
+            print(error)
+        self.__equipment[eq_type].append({name: count})
 
     def transfer_equipment(self, structure, eq_type, name, count):
+        tmp = deepcopy(self.__equipment[eq_type])
+        max_count = tmp[-1].get(name)
+        try:
+            if not count.isdigit():
+                raise CountIsNotDigit
+            else:
+                count = int(count)
+                if count > max_count:
+                    raise CountOutOfLimit
+        except CountIsNotDigit as error:
+            print(error)
+        except CountOutOfLimit as error:
+            print(error)
         if structure not in self.__structure_equipment:
             self.__structure_equipment.update({structure: {}})
         if eq_type not in self.__structure_equipment[structure]:
             self.__structure_equipment[structure].update({eq_type: []})
-        self.__structure_equipment[structure][eq_type].append([name, count])
+        self.__structure_equipment[structure][eq_type].append({name: count})
 
 class OfficeEquipment(ABC):
 
@@ -106,7 +135,7 @@ class Scanner(OfficeEquipment):
     def count(self):
         return f'{self.__count}'
 
-    def add_item(self, **kwargs):
+    def add_itm(self, **kwargs):
        self.__dict__.update(kwargs)    
 
 class Copier(OfficeEquipment):
@@ -128,15 +157,18 @@ class Copier(OfficeEquipment):
     def count(self):
         return f'{self.__count}'
 
-    def add_item(self, **kwargs):
+    def add_itm(self, **kwargs):
        self.__dict__.update(kwargs)  
 
 my_printer = Printer(name = 'epson', count = 5, is_color = True, xerox = False)
 print(my_printer.type)
 my_storehouse = Storehouse()
 my_storehouse.add_equipment(my_printer.type, my_printer.name, my_printer.count)
+my_storehouse.transfer_equipment('бухгалтерия',my_printer.type, my_printer.name, '2' )
+my_scan = Scanner(name = 'xerox', count = '2', connect = 'WI-FI')
+my_storehouse.add_equipment(my_scan.type, my_scan.name, my_scan.count)
+my_storehouse.transfer_equipment('бухгалтерия',my_scan.type, my_scan.name, '3' )
 print(my_storehouse.get_equip())
-my_storehouse.transfer_equipment('бухгалтерия',my_printer.type, my_printer.name, 2 )
 print(my_storehouse.get_struct_equip())
 
 
